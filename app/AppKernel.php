34431e19cc4d7576c5850ce74caa7f0433b92d13
Zachary Mays <zackmays@gmail.com>
2016-01-28 14:55:24 -0800

    Adds user bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new ArtistShuffle\ArtistBundle\ArtistShuffleArtistBundle(),
+            new ArtistShuffle\UserBundle\ArtistShuffleUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/ArtistShuffle/UserBundle/ArtistShuffleUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ArtistShuffle\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ArtistShuffleUserBundle extends Bundle
+{
+}
