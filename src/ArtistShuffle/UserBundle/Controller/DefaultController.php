d8198a7d292289415986793b7e394dbb95d9b8cc
Zachary Mays <zackmays@gmail.com>
2016-01-29 11:00:43 -0800

    Updates login form; puts all but homepage behind security

--- src/ArtistShuffle/UserBundle/Controller/DefaultController.php
+++ src/ArtistShuffle/UserBundle/Controller/DefaultController.php
@@ -9,11 +9,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/users")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return $this->render('ArtistShuffleUserBundle::index.html.twig');
     }
 }
