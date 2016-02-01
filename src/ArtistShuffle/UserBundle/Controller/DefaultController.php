e957a0e7782b277492a23b2fd812e40c450e6de7
Zachary Mays <zackmays@gmail.com>
2016-02-01 11:05:38 -0800

    Adds user add function, updates user homepage

--- src/ArtistShuffle/UserBundle/Controller/DefaultController.php
+++ src/ArtistShuffle/UserBundle/Controller/DefaultController.php
@@ -5,15 +5,49 @@ namespace ArtistShuffle\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use ArtistShuffle\UserBundle\Entity\User;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/users")
+     * @Route("/users", name="users_index")
      * @Template()
      */
     public function indexAction()
     {
-        return $this->render('ArtistShuffleUserBundle::index.html.twig');
+        $users = $this->getDoctrine()->getRepository( 'ArtistShuffleUserBundle:User' )->findAll();
+
+        return $this->render('ArtistShuffleUserBundle::index.html.twig', array( 'users' => $users ));
+    }
+
+
+    /**
+     * @Route("/users/add", name="users_add")
+     * @Template()
+     */
+    public function addUserAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('firstName', 'text')
+            ->add('lastName', 'text')
+            ->add('username', 'text')
+            ->add('email', 'text')
+            ->add('password', 'text')
+            ->add('save', 'submit', array('label' => 'Create User'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ( $form->isValid() )
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('users_index');
+        }
+        return $this->render('ArtistShuffleUserBundle::add.html.twig', array( 'form' => $form->createView() ) );
     }
 }
--- src/ArtistShuffle/UserBundle/Entity/UserRepository.php
+++ src/ArtistShuffle/UserBundle/Entity/UserRepository.php
@@ -10,4 +10,8 @@ namespace ArtistShuffle\UserBundle\Entity;
  */
 class UserRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function findAll()
+    {
+        return $this->findBy(array(), array('firstName' => 'ASC'));
+    }
 }
